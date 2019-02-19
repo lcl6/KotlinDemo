fc95809e69e50b00422c1960c0f636e606d70833
lcl6 <chenlin123456>
2019-02-19 11:56:22 +0800

    添加了权限控制测试

--- app/src/main/java/com/snxun/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/snxun/kotlindemo/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.snxun.kotlindemo
 
-import android.os.Bundle
+import android.Manifest
+import android.annotation.SuppressLint
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
+import com.lodz.android.corekt.utils.ToastUtils
+import com.lodz.android.pandora.base.activity.BaseActivity
+import permissions.dispatcher.*
 
 /**
  * Created by liancl on 2019/2/13.
  *
  */
- class MainActivity : AppCompatActivity(){
-    var mListener: ((String) -> Unit)? =null
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+@RuntimePermissions
+class MainActivity : BaseActivity(){
+    override fun getLayoutId(): Int {
+       return R.layout. activity_main
+    }
 
+    override fun initData() {
+        super.initData()
+        goneTitleBar()
+        showStatusCompleted()
         findViewById<TextView>(R.id.tv_btn).setOnClickListener {
 
             mListener?.invoke("你好")
@@ -23,11 +30,44 @@ import androidx.appcompat.app.AppCompatActivity
         setOnMyClick { mag ->
             Toast.makeText(this,mag,Toast.LENGTH_LONG).show()
         }
+        needWithPermissionCheck()
+
+
     }
+    var mListener: ((String) -> Unit)? =null
 
     fun setOnMyClick(listener: (mag:String)->Unit){
         mListener=listener
     }
+
+
+    @NeedsPermission(Manifest.permission.CAMERA)
+    fun need() {
+        ToastUtils.showShort(getContext(),"need")
+    }
+
+    @SuppressLint("NeedOnRequestPermissionsResult")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        onRequestPermissionsResult(requestCode, grantResults)
+    }
+
+    @OnShowRationale(Manifest.permission.CAMERA)
+    fun showRation(request: PermissionRequest) {
+        request.proceed()
+    }
+
+    @OnPermissionDenied(Manifest.permission.CAMERA)
+    fun deny() {
+        needWithPermissionCheck()
+    }
+
+    @OnNeverAskAgain(Manifest.permission.CAMERA)
+    fun neverAsk() {
+    }
+
+
+
 
 
 }
---
+++ app/src/main/java/com/snxun/kotlindemo/SplashActivity.java
@@ -0,0 +1,11 @@
+package com.snxun.kotlindemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by liancl on 2019/2/19.
+ */
+public class SplashActivity  extends AppCompatActivity {
+
+}
+
