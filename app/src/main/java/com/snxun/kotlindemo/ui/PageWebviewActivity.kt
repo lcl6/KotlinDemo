5f9eb7a2e2d4817dc3bc274d69d75f215423c2b0
lcl6 <chenlin123456>
2019-03-07 15:19:20 +0800

    弄了个jb玩意

--- app/src/main/java/com/snxun/kotlindemo/ui/PageWebviewActivity.kt
+++ app/src/main/java/com/snxun/kotlindemo/ui/PageWebviewActivity.kt
@@ -5,8 +5,12 @@ import android.content.Intent
 import android.os.Bundle
 import butterknife.BindView
 import butterknife.ButterKnife
+import com.lodz.android.corekt.log.PrintLog
 import com.lodz.android.pandora.base.activity.BaseActivity
 import com.snxun.kotlindemo.R
+import com.snxun.kotlindemo.constant.AppConstant
+import com.snxun.kotlindemo.utils.jsbridge.BridgeLogHandler
+import com.snxun.kotlindemo.utils.jsbridge.CallBackFunction
 import com.snxun.kotlindemo.widget.PgWebView
 
 /**
@@ -44,9 +48,28 @@ class PageWebviewActivity : BaseActivity() {
 
     }
 
+    override fun setListeners() {
+        super.setListeners()
+        //h5给安卓传参数 调用安卓的方法
+        webView.registerData(object :BridgeLogHandler("submitFromWeb"){
+            override fun onHandler(data: String, function: CallBackFunction) {
+                PrintLog.e(AppConstant.TAG,data)
+                function.onCallBack("哈哈我是移动端返回的")
+            }
+        })
+        //将数据传给h5
+        webView.sendData("pageShareMsg","你好",object : CallBackFunction {
+            override fun onCallBack(data: String) {
+                PrintLog.e(AppConstant.TAG,"成功了")
+            }
+        })
+
+
+    }
+
     override fun initData() {
         super.initData()
-        webView.loadUrl("http://www.baidu.com")
+        webView.loadUrl("file:///android_asset/demo.html")
 
     }
 
