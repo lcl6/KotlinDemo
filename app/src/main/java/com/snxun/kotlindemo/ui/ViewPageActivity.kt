6f81dd778c1c1afc86400ad1affb3c61c925ac71
lcl6 <chenlin123456>
2019-03-11 11:45:58 +0800

    弄了个jb玩意

--- app/src/main/java/com/snxun/kotlindemo/ui/ViewPageActivity.kt
+++ app/src/main/java/com/snxun/kotlindemo/ui/ViewPageActivity.kt
@@ -3,6 +3,8 @@ package com.snxun.kotlindemo.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import butterknife.BindView
@@ -29,6 +31,8 @@ class ViewPageActivity : BaseInterActivity() {
     lateinit var tabLayout: TabLayout
 
 
+    var mList= ArrayList<String>()
+    private var mFragmentList = ArrayList<Fragment>()
 
     companion object {
         fun start(context: Context){
@@ -53,10 +57,6 @@ class ViewPageActivity : BaseInterActivity() {
     }
 
     private fun initViewPage() {
-
-       var mList= ArrayList<String>()
-       var mFragmentList = ArrayList<Fragment>()
-
         mList.add("首页")
         mList.add("朋友圈")
         mList.add("我的")
@@ -73,6 +73,7 @@ class ViewPageActivity : BaseInterActivity() {
         viewPager.offscreenPageLimit=mList.size
         tabLayout.tabMode=TabLayout.MODE_FIXED
         tabLayout.setupWithViewPager(viewPager)
+        addTabNum("1","2","3")
 
     }
 
@@ -88,22 +89,50 @@ class ViewPageActivity : BaseInterActivity() {
         listener=object:Listener{
             override fun npnr(npnr:String) {
                 ToastUtils.showShort(getContext(),"调用了npnr")
+                addTabNum("6","7","8")
             }
 
             override fun wpnr(wpnr:String) {
                 ToastUtils.showShort(getContext(),"调用了wpnr")
+                addTabNum("7","8","9")
             }
 
             override fun wpwr(param:Any,wpwr:String) {
                 ToastUtils.showShort(getContext(),"调用了wpwr"+param?.toString())
+                addTabNum("11","12","14")
             }
 
             override fun npwr(npwr:String) {
+                addTabNum("15","16","17")
                 ToastUtils.showShort(getContext(),"调用了npwr")
             }
 
         }
     }
+
+
+    fun addTabNum( mainNum:String,pyqNum:String,myNum:String){
+
+        for (index in 0 until mList.size){
+            tabLayout.getTabAt(index)!!.customView = null
+            val view = LayoutInflater.from(getContext()).inflate(R.layout.tab_item, null)
+            val tvTitle = view.findViewById<TextView>(R.id.tv_title)
+            val tvSum = view.findViewById<TextView>(R.id.tv_sum)
+            when(index){
+               0 -> {
+                    tvTitle.text = "首页"; tvSum.text = mainNum
+                }
+                1 -> {
+                    tvTitle.text = "朋友圈"; tvSum.text = pyqNum
+                }
+                2 -> {
+                    tvTitle.text = "我的"; tvSum.text = myNum
+                }
+            }
+            tabLayout.getTabAt(index)!!.customView = view
+        }
+    }
+
 
 
 }
